5960d563686e881ee7d45931064958af004fade8
Yuwipe <yullipe.kun@gmail.com>
2024-09-26 23:08:17 +0800

    Update
    Fixed background
    Added text background

--- scipt.js
+++ scipt.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-let slides = document.querySelectorAll('.slides.fade');
-let slide_index = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    let slides = document.querySelectorAll('.slides.fade');
+    let slide_index = 0;
 
-function show_slides() {
-    slides.forEach(function(slide) {
-        slide.classList.remove('active');
-    });
+    function show_slides() {
+        slides.forEach(function(slide) {
+            slide.classList.remove('active');
+        });
 
-    slide_index++;
-    if (slide_index >= slides.length) {
-        slide_index = 0;
-    }
+        slide_index++;
+        if (slide_index >= slides.length) {
+            slide_index = 0;
+        }
 
-    slides[slide_index].classList.add('active');
+        slides[slide_index].classList.add('active');
     }
     show_slides();
-    setInterval(show_slides, 5000);
+    setInterval(show_slides, 5000); 
 });
